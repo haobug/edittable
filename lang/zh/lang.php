b1bba1a594f71ffc21404fa7634f349788343c8e
haobug <qingxianhao@gmail.com>
2013-06-12 04:42:05 +0800

    add simplified chinese strings

---
+++ lang/zh/lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['toggle_header']    = '切换是否为表头';
+$lang['val_align_left']   = '单元格内容左对齐';
+$lang['val_align_center'] = '单元格内容居中对齐';
+$lang['val_align_right']  = '单元格内容右对齐';
+$lang['span_col_plus']    = '增加水平合并单元格';
+$lang['span_col_minus']   = '减少水平合并单元格';
+$lang['span_row_plus']    = '增加垂直合并单元格';
+$lang['span_row_minus']   = '减少垂直合并单元格';
+$lang['secedit_name']     = '表格';
+$lang['js']['confirmdeleterow'] = '确定删除行？';
+$lang['js']['confirmdeletecol'] = '确定删除列？';
+$lang['js']['struct_col_plus']  = '在右边添加列';
+$lang['js']['struct_col_minus'] = '删除列';
+$lang['js']['struct_col_plus_before']  = '在左边添加列';
+$lang['js']['struct_row_plus']  = '在下面添加行';
+$lang['js']['struct_row_minus'] = '删除行';
+$lang['js']['struct_row_plus_before']  = '在上面添加行';
+$lang['js']['last_column'] = '不能删除最后一列！';
+$lang['js']['last_row'] = '不能删除最后一行！';
+$lang['add_table'] = '插入一个表格';
